588d6ca5164961c768cc7f778e2e25d7a586dba6
andresalberto94 <gomezandres331@gmail.com>
2023-10-22 15:17:00 +0300

    pool request

--- database/database.js
+++ database/database.js
@@ -1,13 +1,31 @@
 
-
 import { postgres } from "../deps.js";
+import { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
-let sql;
-if (Deno.env.get("DATABASE_URL")) {
-  sql = postgres(Deno.env.get("DATABASE_URL"));
-} else {
-  sql = postgres({});
-}
+const CONCURRENT_CONNECTIONS = 2;
+const connectionPool = new Pool({}, CONCURRENT_CONNECTIONS);
 
+const executeQuery = async (query, params) => {
+  const response = {};
+  let client;
 
-export {sql};
+  try {
+    client = await connectionPool.connect();
+    const result = await client.queryObject(query, params);
+    if (result.rows) {
+      response.rows = result.rows;
+    }
+  } catch (e) {
+    response.error = e;
+  } finally {
+    try {
+      await client.release();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return response;
+};
+
+export { executeQuery };
