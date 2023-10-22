f17c90a965121d3bef4e36f741fb435625f879ad
andresalberto94 <gomezandres331@gmail.com>
2023-10-22 13:04:09 +0300

    pool remove

--- database/database.js
+++ database/database.js
@@ -10,30 +10,5 @@ if (Deno.env.get("DATABASE_URL")) {
 }
 
 
-const CONCURRENT_CONNECTIONS = 2;
-const connectionPool = new Pool({}, CONCURRENT_CONNECTIONS);
 
-const executeQuery = async (query, params) => {
-  const response = {};
-  let client;
-
-  try {
-    client = await connectionPool.connect();
-    const result = await client.queryObject(query, params);
-    if (result.rows) {
-      response.rows = result.rows;
-    }
-  } catch (e) {
-    response.error = e;
-  } finally {
-    try {
-      await client.release();
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  return response;
-};
-
-export { executeQuery, sql};
+export {sql};
