4094870f7e53971ca4d0ab01992958c9814e545b
andresalberto94 <gomezandres331@gmail.com>
2023-10-22 15:12:03 +0300

    lsa

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 
-import { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+
 import { postgres } from "../deps.js";
 
 let sql;
@@ -10,36 +10,4 @@ if (Deno.env.get("DATABASE_URL")) {
 }
 
 
-const CONCURRENT_CONNECTIONS = 2;
-const connectionPool = new Pool({
-  hostname: "artos_unique_wsd_deployment_iii",
-  database: "ru8s",
-  user: "ybY3r9Q8IRfTpmpQSHlquxeG0f7YShyG@dpg-ck9ut1ns0fgc73frcuk0-a/artos_unique_wsd_deployment_iii_database_ru8s",
- 
-}, CONCURRENT_CONNECTIONS);
-
-
-const executeQuery = async (query, params) => {
-  const response = {};
-  let client;
-
-  try {
-    client = await connectionPool.connect();
-    const result = await client.queryObject(query, params);
-    if (result.rows) {
-      response.rows = result.rows;
-    }
-  } catch (e) {
-    response.error = e;
-  } finally {
-    try {
-      await client.release();
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  return response;
-};
-
-export { executeQuery, sql };
+export {sql};
