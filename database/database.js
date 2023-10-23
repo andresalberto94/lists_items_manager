4ba863b23aaded402435684d0589fe0002246026
andresalberto94 <gomezandres331@gmail.com>
2023-10-23 14:09:38 +0300

    dfvdf

---
+++ database/database.js
@@ -0,0 +1,13 @@
+
+
+import { postgres } from "../deps.js";
+
+let sql;
+if (Deno.env.get("DATABASE_URL")) {
+  sql = postgres(Deno.env.get("DATABASE_URL"));
+} else {
+  sql = postgres({});
+}
+
+
+export {sql};
