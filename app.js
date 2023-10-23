f3f9f4d0da88b5dfea8ce6fbadd089ca833f15d3
andresalberto94 <gomezandres331@gmail.com>
2023-10-23 13:26:35 +0300

    vdfvdf

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const handleRequest = async (request) => {
     return await listsController.addList(request);
   } else if (url.pathname === "/lists" && request.method === "GET") {
     return await listsController.viewLists(request);
-  } else if (url.pathname.match("lists/[0-9]+/desactive") && request.method === "POST") {
-    return await listsController.desactive(request);
+  } else if (url.pathname.match("lists/[0-9]+/deactive") && request.method === "POST") {
+    return await listsController.deactive(request);
   } else if (url.pathname.match("/lists/[0-9]+") && request.method === "GET") {
     return await listsController.viewList(request);
   } else if (url.pathname === "/" && request.method === "GET") {
